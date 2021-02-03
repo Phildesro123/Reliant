6e2cec6f30247342b7afab580bc40eb23c41d5b7
Youssef Asaad <youssef.a.asaad@gmail.com>
2021-02-03 15:23:27 -0500

    edited functionality to highlight all paragraphs

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,4 +3,19 @@ import { printLine } from './modules/print';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 printLine("Using the 'printLine' function from the Print Module");
+let paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs)
+for (const paragraph of paragraphs) {
+    console.log(paragraph)
+    paragraph.style['background-color'] = getRandomColor();
+}
