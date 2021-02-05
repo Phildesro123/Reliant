247e72ebe7f28fd25079eb1f7cd5b3a1a0c05774
Cooper Meitz <coopermeitz@gatech.edu>
2021-02-05 10:34:57 -0500

    Extension alternates between original color and one random color. Transparent icon

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -14,16 +14,30 @@ function getRandomColor() {
   return color;
 }
 
+var first = true;
+var colors = []; // Array holding paragraph colors in the form [original, random]
+var even = 0; // 0 --> Original Color, 1 --> Random Color
 chrome.runtime.onMessage.addListener((req, send, sendResponse) => {
+  even = (even + 1) % 2;
   console.log(
     send.tab ? 'from a content script:' + send.tab.url : 'from the extension'
   );
   let paragraphs = document.getElementsByTagName('p');
   console.log(paragraphs);
+  var i = 0;
+  console.log(colors);
   for (const paragraph of paragraphs) {
     //console.log(paragraph.textContent)
-    paragraph.style['background-color'] = getRandomColor();
+    if (first) {
+      colors.push([paragraph.style['background-color'], getRandomColor()]);
+      console.log(colors[i]);
+      paragraph.style['background-color'] = colors[i][1];
+    } else {
+      paragraph.style['background-color'] = colors[i][even];
+    }
+    i++;
   }
+  first = false;
   sendResponse({ message: 'ACK' });
 });
 printLine("Using the 'printLine' function from the Print Module");
