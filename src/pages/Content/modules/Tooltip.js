1dff0802521d24d958cb041601cd47689f5a8f2d
unknown <phildesro@yahoo.com>
2021-03-11 17:40:15 -0500

    Removed unncessary imports

--- src/pages/Content/modules/Tooltip.js
+++ src/pages/Content/modules/Tooltip.js
@@ -7,27 +7,6 @@ import {FaExclamation} from 'react-icons/fa';
 import {FaComment} from 'react-icons/fa';
 import {FaStickyNote} from 'react-icons/fa';
 
-
-import Popover from 'react-bootstrap/Popover';
-
-
-
-const popover = (
-    <Popover style={{backgroundColor: "red"}} id="popover-basic">
-      {/* <Popover.Title as="h3">Options</Popover.Title> */}
-      <Popover.Content>
-        {/* And here's some <strong>amazing</strong> content. It's very engaging.
-        right? */}
-
-        {/* <Button id='smile' onClick={()=>console.log("smile")}> <FaRegSmile /></Button>
-        <Button id='frown' onClick={()=>console.log("frown")}> <FaRegFrown/></Button>
-        <Button id='highlight' onClick={()=>console.log("highlight")}> <FaExclamation/></Button>
-        <Button id='comment' onClick={()=>console.log("comment")}> <FaComment/></Button>
-        <Button id='note' onClick={()=>console.log("note")}> <FaStickyNote/></Button> */}
-      </Popover.Content>
-    </Popover>
-  );
-
 const ToolTip = () => {
     return (
       // <Popover style={{backgroundColor: "red"}} id="popover-basic">
