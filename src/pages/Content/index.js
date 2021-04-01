f6543c7f211fc17d02f8114b97a2431c0442cc02
Avinash Sivakumar <avin1100@gmail.com>
2021-04-01 12:59:58 -0400

    fixed tooltip positioning

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -244,7 +244,7 @@ async function activateReliant() {
 
     const renderToolTip = (mouseX, mouseY, selection) => {
       mouseX = mouseX - 50;
-      mouseY = mouseY - 40;
+      mouseY = mouseY - 30;
       if (selection.length < 60) {
         mouseX = mouseX - 20;
       }
@@ -281,6 +281,7 @@ async function activateReliant() {
       //Make the tool tip invisible
       if (isToolTipVisible) {
         e.stopPropagation();
+        closeToolTip();
         return false;
       } else {
         startX = e.pageX;
@@ -288,6 +289,29 @@ async function activateReliant() {
         closeToolTip();
       }
     });
+
+    function getSelectionDimensions() {
+      var sel = document.selection, range;
+      var width = 0, height = 0;
+      if (sel) {
+          if (sel.type != "Control") {
+              range = sel.createRange();
+              width = range.boundingWidth;
+              height = range.boundingHeight;
+          }
+      } else if (window.getSelection) {
+          sel = window.getSelection();
+          if (sel.rangeCount) {
+              range = sel.getRangeAt(0).cloneRange();
+              if (range.getBoundingClientRect) {
+                  var rect = range.getBoundingClientRect();
+                  width = rect.right - rect.left;
+                  height = rect.bottom - rect.top;
+              }
+          }
+      }
+      return { width: width , height: height };
+    }
     // Show the tool tip
     let paragraphs = document.getElementsByTagName('p');
     document.addEventListener('mouseup', (e) => {
@@ -311,6 +335,15 @@ async function activateReliant() {
         return false;
       } else if (selection.length > 0) {
         //Render the tooltip
+
+        console.log("SELECTION IS THIS WE TRYHING");
+        // h = getSelectionDimensions()
+        console.log(getSelectionDimensions());
+        var h = getSelectionDimensions().height;
+        console.log("WE TREID LMAOOOOOOOOO");
+
+
+
         endX = e.pageX;
         endY = e.pageY;
         console.log('start x is ', startY);
@@ -326,7 +359,7 @@ async function activateReliant() {
 
         renderToolTip(
           (realendX - realStartX) / 2 + realStartX,
-          realStartY - (realEndY - realStartY) / 2,
+          realEndY - h,
           selection
         );
       } else {
