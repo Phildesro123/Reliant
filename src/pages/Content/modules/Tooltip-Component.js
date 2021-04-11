d57fcbe52f0a6a06166c801ac6c34dff242cac60
unknown <phildesro@yahoo.com>
2021-04-11 15:30:30 -0400

    Fixed tooltip positioning issue

--- src/pages/Content/modules/Tooltip-Component.js
+++ src/pages/Content/modules/Tooltip-Component.js
@@ -25,11 +25,11 @@ export function removeTooltip() {
 const ToolTip = () => {
     return (        
         <> 
-         <button type="button" className="btn btn-success" id='smile' title="Green Highlight" onClick={()=>console.log("smile")}> <FaRegSmile id = "smile"/></button>
-          <button type="button" className="btn btn-danger" id='frown' title="Red Highlight" onClick={()=>console.log("frown")}> <FaRegFrown id ="frown"/></button>
-          <button type="button" className="btn btn-warning" id='highlight' title="Yellow Highlight" onClick={()=>console.log("highlight")}> <FaExclamation id ="highlight"/></button>
-          <button type="button" className="btn btn-primary" id='comment' title="Create comment" onClick={()=>console.log("comment")}> <FaComment id = "comment"/></button>
-          <button type="button" className="btn btn-dark" id='note' title="Personal Note" onClick={()=>console.log("note")}> <FaStickyNote id ="note"/></button>
+         <button type="button" className="btn btn-success" id='button-fix' title="Green Highlight" onClick={()=>console.log("smile")}> <FaRegSmile id = "smile"/></button>
+          <button type="button" className="btn btn-danger" id='button-fix' title="Red Highlight" onClick={()=>console.log("frown")}> <FaRegFrown id ="frown"/></button>
+          <button type="button" className="btn btn-warning" id='button-fix' title="Yellow Highlight" onClick={()=>console.log("highlight")}> <FaExclamation id ="highlight"/></button>
+          <button type="button" className="btn btn-primary" id='button-fix' title="Create comment" onClick={()=>console.log("comment")}> <FaComment id = "comment"/></button>
+          <button type="button" className="btn btn-dark" id='button-fix' title="Personal Note" onClick={()=>console.log("note")}> <FaStickyNote id ="note"/></button>
         </>
     );
     
