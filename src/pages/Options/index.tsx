ebea944425593155b5dcff6737d3800b736d7ea1
pdesrosiers3@gatech.edu <phildesro@yahoo.com>
2024-06-27 16:06:37 -0400

    Updated dependencies, boilerplate, and some imports

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+//const domNode = document.getElementById('root');
+//const root = createRoot(domNode);
+
 import Options from './Options';
 import './index.css';
 
